7edf5f50a29960521f201b35b93874f3a005bfaf
Chirag <chiragbangera007@gmail.com>
2024-03-10 00:45:14 +0530

    char datatype practice

---
+++ lib/src/main/java/miscJavaPractice/CharacterPractice.java
@@ -0,0 +1,11 @@
+package miscJavaPractice;
+
+public class CharacterPractice {
+
+	public static void main(String[] args) {
+		char tempChar = '3';
+		char tempChar2 = (char) (tempChar ^ 3);
+		System.out.println(tempChar2);
+	}
+	
+}
